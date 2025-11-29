def11e644eae780e36860b7a113d171d1f09093c
Princek <princestudy.7498@gmail.com>
2025-11-29 20:28:10 +0000

    conversion of datatypes

---
+++ basics/03_conversionopperation.js
@@ -0,0 +1,19 @@
+let Score =true;
+
+console.log(typeof Score);
+
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+
+
+//"33" =>33
+//"33abc" => NaN
+//true =>1 ; false =>0;
+
+let isLoggedIn="Prince"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0=>false
+//"" => false
+//"prince" => true
