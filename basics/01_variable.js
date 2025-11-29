d95f5f7a8313ddc018e0ce1a824446873bc00ee6
Princek <princestudy.7498@gmail.com>
2025-11-29 19:57:30 +0000

    basics+variable+datatypes

---
+++ basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId =12345;
+let accountEmail="goundprince@gamil.com";
+var accountPassword="password";
+accountcity="jaipur";
+let accountState;
+
+// console.log(accountId);
+accountEmail ="hsvs@gamil.com";
+accountPassword=1163131;
+accountcity="banglore";
+
+/*
+preffer not to use var
+because of issue in block scope({}) and functionalscope
+*/
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3); we are using node js not browser
+//code readability is very important
+
+let name="Prince"
+let age=20;
+let isLoggedIn= false;
+
+//number =>2 to power 53
+//bigint
+//String =>""
+//boolean => true or false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof "Prince");  //String
