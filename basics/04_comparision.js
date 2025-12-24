07b9578359f02511aba603726b390e1cb798b01e
Princek <princestudy.7498@gmail.com>
2025-12-24 18:38:47 +0000

    code config

--- basics/04_comparision.js
+++ basics/04_comparision.js
@@ -1,18 +1,22 @@
-// console.log(2>1);
-// console.log(2<=1);
-// console.log(2==2);
+console.log(2>1);
+console.log(2<=1);
+console.log(2==2);
 
 console.log("2">1);
 console.log("02"<1);
+
+// --> from here there are cnfusing comparision 
+
 console.log(null>0);
 console.log(null==0);
 console.log(null<=0);
 console.log(null>=0);
 
-// comparision of undefined
+// // comparision of undefined
 console.log(undefined==0);
-console.log(undefined<=o);
-console.log(undefined>=0);
+// console.log(undefined<=o);  this statement cannot have a output and show error
+console.log(undefined>=0);  
+//                                                          till here  <--//
 
 //===
 console.log("2"===2);
